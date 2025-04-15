25f854b3cca2028b8861bd1b83f8ab88c2bc857f
SSM-F <melaniebc57@gmail.com>
2025-04-15 09:00:16 +0100

    closing connection

--- backend/helper.py
+++ backend/helper.py
@@ -12,28 +12,31 @@ def formated_data(columns,raw_data):
 
 def return_data(file_path):
     data = None
-    if file_path.endswith('csv'):
-        data = read_csv(file_path)
-    if file_path.endswith('json'):
-        data = read_json(file_path)
-
-    query = """SELECT date,description,amount,category FROM expenses
-                WHERE date=:date AND description=:description 
-                AND amount=:amount AND category=:category;
-                """
-    conn= db_connection()
-    
-    rows = data['Invoice_Data']
-
-    data_result = []
-    for row in rows:
-        x = conn.run(query,
-                 date=row['Date'],
-                 description=row['Description'],
-                 amount=row['Amount'],
-                 category = row['Category']) 
+    try:
         
-        data_result.append(x[0])
-    
-    return data_result
-    
+        if file_path.endswith('csv'):
+            data = read_csv(file_path)
+        if file_path.endswith('json'):
+            data = read_json(file_path)
+
+        query = """SELECT date,description,amount,category FROM expenses
+                    WHERE date=:date AND description=:description 
+                    AND amount=:amount AND category=:category;
+                    """
+        conn= db_connection()
+
+        rows = data['Invoice_Data']
+
+        data_result = []
+        for row in rows:
+            x = conn.run(query,
+                     date=row['Date'],
+                     description=row['Description'],
+                     amount=row['Amount'],
+                     category = row['Category']) 
+
+            data_result.append(x[0])
+
+        return data_result
+    finally:
+        close_conn(conn)
