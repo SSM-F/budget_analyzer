0d4dcd51063b5874e25eb7305251f348a507b9ec
SSM-F <melaniebc57@gmail.com>
2025-04-15 07:16:03 +0100

    making function more usable for different scenarios

--- backend/read_csv.py
+++ backend/read_csv.py
@@ -1,17 +1,25 @@
 import csv
 import logging
+from typing import Union
+from fastapi import UploadFile,File
 
 
-def read_csv(csv_file):
+def read_csv(csv_file: Union[str, UploadFile]):
     try:
-        with open(f'{csv_file}', newline='') as file:
+        if isinstance(csv_file,str):
+            with open(f'{csv_file}', newline='') as file:
+                reader = csv.DictReader(file)
+                lines = [line for line in reader]
+                    
+        else:
+            csv_file.file.seek(0) 
+            content = csv_file.file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(content)
+            lines = [line for line in reader]
+
         
-            reader = csv.DictReader(file)
-            lines = []
-            for line in reader:
-                lines.append(line)
-            logging.info('CSV file succesfully read')
-            return {'Invoice_Data': lines}
+        logging.info('CSV file succesfully read')
+        return {'Invoice_Data': lines}
     except Exception:
         raise Exception ('File Not Found')  
             
