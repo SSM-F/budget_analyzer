c7a9e6b8a70c721c3c425c9c5c4b2b35aba0deaa
SSM-F <melaniebc57@gmail.com>
2025-04-19 13:53:29 +0100

    testing HTTPException for endpoint PUT

--- test/test_api.py
+++ test/test_api.py
@@ -4,6 +4,7 @@ from fastapi.testclient import TestClient
 import pytest
 from pprint import pprint
 import json
+import io
 
 @pytest.fixture(autouse=True)
 def reset_db():
@@ -84,6 +85,7 @@ class TestPut:
     
         assert response_decoded == expected_items
 
+
     def test_put_endpoint_works_with_json_files(self,reset_db,test_client):
         test_invoice = 'data/invoice_holidays.json'
         test_table = 'expenses'
@@ -109,11 +111,29 @@ class TestPut:
                         'id': 103
                         }]}
         assert expected == response_decoded
+        assert response.status_code == 201
+
+    def test_put_endpoint_HTTPException(self,reset_db,test_client):
+        test_invoice = 'not_existent_path.yml'
+        fake_file = io.BytesIO(b"not a real json or csv")
+        test_table = 'expenses' 
+        response = test_client.put(f'/api/upload?table_name={test_table}',
+                                   files={'file_path':(test_invoice,fake_file,'text/json')})
+       
+      
+        assert response.status_code == 415
+        assert response.json()['detail'] == 'Unsuported file'
+        
+        
 
 class TestDelete:
     def test_endpoint_delete_status_code(self,reset_db,test_client):
-        test_id= 1
+        test_id= 101
         test_table = 'expenses'
+        test_invoice = 'data/invoice_holidays.json'
+        with open(test_invoice,'rb') as f:
+            response_put = test_client.put(f'/api/upload?table_name={test_table}',
+                                           files={'file_path':(test_invoice,f,'text/json')})
         response = test_client.delete(f'/api/delete/{test_table}/{test_id}')
         assert response.status_code == 200
 
@@ -133,6 +153,14 @@ class TestDelete:
                                                         'date': '2025-07-15',
                                                         'description': 'Airbnb - Barcelona trip',
                                                         'id': 101},]}
+        
+    def test_endpoint_delete_raise_expection(self,reset_db,test_client):
+        test_id = 200000
+        test_table = 'expenses'
+        response = test_client.delete(f'/api/delete/{test_table}/{test_id}')
+        assert response.status_code == 404
+        assert response.json()['detail'] == "Invoice with id number: 200000 not found. Please try different id number"
+        
 
 
     
